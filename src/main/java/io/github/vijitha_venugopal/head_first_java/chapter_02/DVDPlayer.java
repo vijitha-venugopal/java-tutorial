f9a45bc5718f80629490fcc809a7ce69365ad212
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 16:01:04 +0530

    Created DVDPlayer.java in chapter_02 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/DVDPlayer.java
@@ -0,0 +1,25 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_02;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class DVDPlayer {
+    boolean canRecord=false;
+
+    void recordDVD() {
+        System.out.println("DVD recorded");
+    }
+    void playDVD() {
+        System.out.println("Play DVD ");
+    }
+}
+class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+        DVDPlayer d=new DVDPlayer();
+        d.canRecord=true;
+        d.playDVD();
+        if(d.canRecord==true){
+            d.recordDVD();
+        }
+    }
+}
