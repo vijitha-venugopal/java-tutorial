95f70044dd5032b4e0ba96207d6c813ffcf27a18
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 15:24:27 +0530

    Create MyfirstApp.java class inside chapter0 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/Movie.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/Movie.java
@@ -9,26 +9,12 @@ public class Movie {
     int rating;
 
     void playIt() {
-        System.out.println("Playing the movie");
+        System.out.println("Playting the movie");
     }
 }
 class MovieTestDrive {
     public static void main(String[] args) {
         Movie one=new Movie();
-        one.title="Gone with the stock";
-        one.genre="Tragic";
-        one.rating=-2;
-
-        Movie two=new Movie();
-        two.title="Lost in cubicle space";
-        two.genre="Commedy";
-        two.rating=5;
-        two.playIt();
-
-        Movie three=new Movie();
-        three.title="Byte club";
-        three.genre="Tragic but ultimately uplifting";
-        three.rating=127;
+        one.playIt();
     }
-
 }
