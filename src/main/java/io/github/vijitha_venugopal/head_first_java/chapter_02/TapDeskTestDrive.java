cfd03b8547b0ed545910a12a92fb9a99fbea3226
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 17:10:46 +0530

    TapDesk.java is renamed as TapDeskTestDrive

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/TapDeskTestDrive.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/TapDeskTestDrive.java
@@ -3,7 +3,20 @@ package io.github.vijitha_venugopal.head_first_java.chapter_02;
 /**
  * Created by vijitha on 03/08/16.
  */
-public class TapeDeck {
+
+public class TapDeskTestDrive {
+    public static void main(String[] args) {
+        TapeDeck t=new TapeDeck();
+        t.pauseTape();
+        t.canRecord=true;
+        t.playTape();
+        if(t.canRecord==true) {
+            t.recordTape();
+        }
+
+    }
+}
+class TapeDeck {
     boolean canRecord=false;
 
     void playTape() {
@@ -14,17 +27,5 @@ public class TapeDeck {
     }
     void pauseTape() {
         System.out.println("Tape Pausing");
-    }
-}
-class TapDeskTestDrive {
-    public static void main(String[] args) {
-        TapeDeck t=new TapeDeck();
-        t.pauseTape();
-        t.canRecord=true;
-        t.playTape();
-        if(t.canRecord==true) {
-            t.recordTape();
-        }
-
     }
 }
