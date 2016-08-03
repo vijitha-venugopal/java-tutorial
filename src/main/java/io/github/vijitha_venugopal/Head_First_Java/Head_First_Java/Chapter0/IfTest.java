8a18213ad526d10aa25797a8a916b67e229d3b97
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 12:38:59 +0530

    Create IfTest.java class closes #2

---
+++ src/main/java/io/github/vijitha_venugopal/Head_First_Java/Head_First_Java/Chapter0/IfTest.java
@@ -0,0 +1,14 @@
+package io.github.vijitha_venugopal.Head_First_Java.Head_First_Java.Chapter0;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class IfTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        }
+        System.out.println("This Runs no matter what");
+    }
+}
