aad1cf01122a26c8336a0a076048bbfb7fefb659
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 14:51:23 +0530

    Create Dog.java class inside chapter_02 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/Dog.java
@@ -0,0 +1,20 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_02;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class Dog {
+    int size;
+    String bread;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff!  Ruff!");
+    }
+}
+class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d=new Dog();
+        d.bark();
+    }
+}
