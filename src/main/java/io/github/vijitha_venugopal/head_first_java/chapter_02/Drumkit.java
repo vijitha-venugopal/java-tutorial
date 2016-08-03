caf0c122b526c848ffcfd8ab07a94f13b89fc2bd
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 16:14:12 +0530

    created Drumkit.java in chapter_02 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/Drumkit.java
@@ -0,0 +1,29 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_02;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class Drumkit {
+    boolean topHat=true;
+    boolean snare=true;
+
+    void playTopHat() {
+        System.out.println("ding ding da-ding");
+    }
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+}
+class DrumkitTestDrive {
+    public static void main(String[] args) {
+        Drumkit d=new Drumkit();
+        d.playSnare();
+        d.snare=false;
+        d.playTopHat();
+        if(d.snare==true){
+            d.playSnare();
+        }
+    }
+}
+
+
