2300b9714491b11bf7ab77fd26703dcb033c7105
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 14:35:33 +0530

    Create Exerciselb.java class inside chapter_01 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_01/Exerciselb.java
@@ -0,0 +1,16 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_01;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class Exerciselb {
+    public static void main(String[] args) {
+        int x=1;
+        while (x<10) {
+            x=x+1;
+            if (x>3) {
+                System.out.println("big x");
+            }
+        }
+    }
+}
