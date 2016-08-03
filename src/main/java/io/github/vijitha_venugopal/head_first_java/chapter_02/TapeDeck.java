69059a6381099e2d77094d4576735f4e99936b71
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 15:49:36 +0530

    Create TapeDeck.java class inside chapter_02 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/TapeDeck.java
@@ -0,0 +1,25 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_02;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class TapeDeck {
+    boolean canRecord=false;
+
+    void playTape() {
+        System.out.println("Tape Playing");
+    }
+    void recordTape() {
+        System.out.println("Tape Recording");
+    }
+}
+class TapDeskTestDrive {
+    public static void main(String[] args) {
+        TapeDeck t=new TapeDeck();
+        t.canRecord=true;
+        t.playTape();
+        if(t.canRecord==true) {
+            t.recordTape();
+        }
+    }
+}
