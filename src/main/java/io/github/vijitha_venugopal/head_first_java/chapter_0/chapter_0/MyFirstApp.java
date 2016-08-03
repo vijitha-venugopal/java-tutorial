51ae6d81c7a58b5e0c433952a09807e68e50d00f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 12:54:01 +0530

    Create MyfirstApp.java class inside chapter0 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_0/chapter_0/MyFirstApp.java
@@ -0,0 +1,13 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_0.chapter_0;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class MyFirstApp {
+    public static void main(String[] args) {
+
+        System.out.println("I Rule");
+        System.out.println("The World");
+    }
+}
+
