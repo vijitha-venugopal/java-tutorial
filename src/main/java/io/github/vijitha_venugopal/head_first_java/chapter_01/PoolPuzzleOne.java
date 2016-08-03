0503ca78e598d220971d3b1a1d624826c279e94b
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 14:41:19 +0530

    Create PoolpuzzleOne.java class inside chapter_01 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_01/PoolPuzzleOne.java
@@ -0,0 +1,29 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_01;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x=0;
+        while (x<4) {
+            System.out.print("a");
+            if(x<1) {
+                System.out.print(" ");
+            }
+            System.out.print("n");
+            if(x>1) {
+                System.out.print(" oyster ");
+                x=x+2;
+            }
+            if(x==1) {
+                System.out.print(" noys ");
+            }
+            if(x<1) {
+                System.out.print(" oise ");
+            }
+            System.out.print(" ");
+            x=x+1;
+        }
+    }
+}
