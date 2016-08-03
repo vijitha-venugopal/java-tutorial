55cbdce62d95122f94a43ed5b3ca9b9c6e372f70
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 13:07:24 +0530

    Create IfTest2.java class inside chapter0 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_0/IfTest2.java
@@ -0,0 +1,16 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_0;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class IfTest2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is not 3");
+        }
+        System.out.println("This Runs no matter what");
+    }
+}
