6ba4292e449e0fbde5dd1ba3d4336f7d96167468
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 11:23:23 +0530

    Added MethodTutorial closes#1

---
+++ src/main/java/io/github/vijitha_venugopal/MethodTutorial.java
@@ -0,0 +1,15 @@
+package io.github.vijitha_venugopal;
+
+/**
+ * Class to test the way Methods Work
+ * Created by vijitha on 03/08/16.
+ */
+public class MethodTutorial {
+    public static void main(String[] args) {
+        System.out.println("Sum : " + sum(5,6));
+    }
+
+    private static int sum(int i, int i1) {
+        return i + i1;
+    }
+}
