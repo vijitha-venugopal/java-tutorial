fbec9555f603ad4eefe44f635a90303fa05d09b9
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 15:54:47 +0530

    Update tapeDeck.java

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/TapeDeck.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/TapeDeck.java
@@ -12,14 +12,19 @@ public class TapeDeck {
     void recordTape() {
         System.out.println("Tape Recording");
     }
+    void pauseTape() {
+        System.out.println("Tape Pausing");
+    }
 }
 class TapDeskTestDrive {
     public static void main(String[] args) {
         TapeDeck t=new TapeDeck();
+        t.pauseTape();
         t.canRecord=true;
         t.playTape();
         if(t.canRecord==true) {
             t.recordTape();
         }
+
     }
 }
