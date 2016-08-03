60fc07c80af80802081b912e3a6f6a6d453203d5
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 14:28:29 +0530

    Create Shuffle1.java class inside chapter_01 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_01/Shuffle1.java
@@ -0,0 +1,24 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_01;
+
+/**
+ * Created by vijitha on 03/08/16.
+ */
+public class Shuffle1 {
+    public static void main(String[] args) {
+        int x=3;
+        while (x>0) {
+            if(x>2) {
+                System.out.print("a");
+            }
+            x=x-1;
+            System.out.print("-");
+            if(x==2) {
+                System.out.print("b c");
+            }
+            if(x==1) {
+                System.out.print("d");
+                x=x-1;
+            }
+        }
+    }
+}
