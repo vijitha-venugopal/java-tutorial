732fde319a7dcea52abd229d7344cdd77226decf
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-03 17:12:30 +0530

    Movie.java is renamed as MovieTestDrive

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/MovieTestDrive.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_02/MovieTestDrive.java
@@ -3,16 +3,7 @@ package io.github.vijitha_venugopal.head_first_java.chapter_02;
 /**
  * Created by vijitha on 03/08/16.
  */
-public class Movie {
-    String title;
-    String genre;
-    int rating;
-
-    void playIt() {
-        System.out.println("Playting the movie");
-    }
-}
-class MovieTestDrive {
+public class MovieTestDrive {
     public static void main(String[] args) {
         Movie one=new Movie();
         one.playIt();
@@ -21,3 +12,13 @@ class MovieTestDrive {
 
     }
 }
+class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Playting the movie");
+    }
+}
+
