74456d71f78979699ec4d6ab11529adb36c68e56
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 14:50:17 +0530

    Updated ForLoop.java  in chapter_05 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/ForLoop.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/ForLoop.java
@@ -7,11 +7,11 @@ package io.github.vijitha_venugopal.head_first_java.chapter_5;
 public class ForLoop {
     public static void main(String[] args) {
         System.out.println("Start");
-        System.out.println(" ");
+        System.out.println("  ");
         for(int i=1;i<=10;i++) {
             System.out.println(i);
         }
-        System.out.println(" ");
+        System.out.println("  ");
         System.out.println("Finish");
     }
 }
