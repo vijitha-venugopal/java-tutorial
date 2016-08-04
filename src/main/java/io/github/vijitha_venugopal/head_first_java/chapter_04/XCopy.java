88cd0a9e88b940edb7918f8f012e648fbf88b4b2
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 12:55:14 +0530

    Updated PoorDogTestDrive.java  in chapter_04 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_04/XCopy.java
@@ -0,0 +1,19 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_04;
+
+/**
+ * class to learn call by value
+ * Created by vijitha on 04/08/16.
+ */
+public class XCopy {
+    public static void main(String[] args) {
+        int orig=45;
+        XCopy a=new XCopy();
+        int y=a.go(orig);
+        System.out.println(orig+" " + y);
+    }
+
+    private int go(int arg) {
+        arg=arg*2;
+        return arg;
+    }
+}
