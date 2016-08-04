7f04c370ad4d7d1d10d303f212aebcf235828c42
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 15:33:09 +0530

    craeted EnhanceForLoop.java  in chapter_05 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/MultiFor.java
@@ -0,0 +1,22 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_5;
+
+/**
+ * class in exercise
+ * Created by vijitha on 04/08/16.
+ */
+public class MultiFor {
+    public static void main(String[] args) {
+        for(int x=0;x<4;x++){
+            for(int y=4;y>2;y--) {
+                System.out.println(x + " " + y);
+            }
+            if(x==1){
+                    x++;
+
+            }
+        }
+
+    }
+
+}
+
