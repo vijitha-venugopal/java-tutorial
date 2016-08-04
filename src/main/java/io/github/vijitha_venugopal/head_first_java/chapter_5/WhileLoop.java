62951828cc64aad77a3268a9b6f2c3dad0b82a39
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 14:49:30 +0530

    craeted While.java  in chapter_05 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/WhileLoop.java
@@ -0,0 +1,19 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_5;
+
+/**
+ * class using while loop
+ * Created by vijitha on 04/08/16.
+ */
+public class WhileLoop {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        System.out.println(" ");
+        int i=1;
+        while(i<=10) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println(" ");
+        System.out.println("Finish");
+    }
+}
