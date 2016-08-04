6222101e9d22932b4d1646df44c1d7c7e2046b45
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 15:01:50 +0530

    craeted EnhanceForLoop.java  in chapter_05 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/EnhanceForLoop.java
@@ -0,0 +1,15 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_5;
+
+/**
+ * class using enhanced for loop
+ * Created by vijitha on 04/08/16.
+ */
+public class EnhanceForLoop {
+    public static void main(String[] args) {
+        int oddnumbers[] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
+
+        for (int t : oddnumbers) {
+            System.out.println(t);
+        }
+    }
+}
