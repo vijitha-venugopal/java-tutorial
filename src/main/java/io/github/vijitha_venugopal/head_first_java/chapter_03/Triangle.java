5da03d64e40adfbbb60383493a0fb4b0df064d43
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 10:42:29 +0530

    Updated Triangle.java  in chapter_03 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_03/Triangle.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_03/Triangle.java
@@ -1,19 +1,20 @@
 package io.github.vijitha_venugopal.head_first_java.chapter_03;
 
 /**
+ * class for test arrays
  * Created by vijitha on 03/08/16.
  */
 public class Triangle {
-    double area;
-    int length;
-    int height;
+    private double area;
+    private int length;
+    private int height;
 
     public static void main(String[] args) {
         int x=0;
         Triangle[] ta =new Triangle[4];
         while(x<4) {
             ta[x]=new Triangle();
-            ta[x].height=(x+1)*1;
+            ta[x].height= (x + 1);
             ta[x].length=x+4;
             ta[x].setArea();
             System.out.print("triangle "+x+", area");
@@ -21,11 +22,11 @@ public class Triangle {
             x+=1;
         }
         int y=x;
-        x=27;
-        Triangle t5=ta[2];
+        Triangle t5 = ta[2];
         ta[2].area=343;
         System.out.print("y = "+y);
         System.out.println(", t5 area = "+t5.area);
+
     }
 
     private void setArea() {
