0c6688c42f88d844b8073637ac5f35618a27c0e7
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 14:46:39 +0530

    craeted ForLoop.java  in chapter_04 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/ForLoop.java
@@ -0,0 +1,17 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_5;
+
+/**
+ * class using for loop
+ * Created by vijitha on 04/08/16.
+ */
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        System.out.println(" ");
+        for(int i=1;i<=10;i++) {
+            System.out.println(i);
+        }
+        System.out.println(" ");
+        System.out.println("Finish");
+    }
+}
