9971dfb775e44e50c93d7cd32d9bbfdeb45ff36f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-04 15:22:54 +0530

    craeted Output.java  in chapter_05 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_5/Output.java
@@ -0,0 +1,27 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_5;
+
+/**
+ * class find output of program
+ * Created by vijitha on 04/08/16.
+ */
+public class Output {
+    public static void main(String[] args) {
+        Output o=new Output();
+        o.go();
+    }
+
+    private void go() {
+        int y=7;
+        for(int x=1;x<8;x++) {
+            y++;
+            if(x>4) {
+                System.out.print(++y + " ");
+            }
+            if(y>14) {
+                System.out.println("x = " +x);
+                break;
+            }
+        }
+    }
+
+}
