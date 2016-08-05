16136d45c434d1df244e18d6d6fa6ab03c0144a5
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-05 10:06:06 +0530

    Updated Mixed.java  in chapter_07 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_07/TestBoats.java
@@ -0,0 +1,39 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_07;
+
+/**
+ * Created by vijitha on 05/08/16.
+ */
+public class TestBoats {
+    public static void main(String[] args) {
+       Boat b1=new Boat();
+        Sailboat b2=new Sailboat();
+        Rowboat b3=new Rowboat();
+//        b2.setLength(32);
+        b1.move();
+        b3.move();
+        b2.move();
+
+    }
+}
+class Boat {
+    private int length;
+    public void setLength(int len) {
+        length=len;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void move() {
+        System.out.print("drift ");
+    }
+}
+class Rowboat extends Boat {
+    public void rowTheBoat() {
+        System.out.print("stroke natasha");
+    }
+}
+class Sailboat extends Boat {
+    public void move() {
+        System.out.print("hoist sail ");
+    }
+}
