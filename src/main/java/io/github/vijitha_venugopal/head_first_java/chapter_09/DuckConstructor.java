ce3ac3a6a6875ea986eeb1602f64ea9be8115ece
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-05 14:46:36 +0530

    Updated DuckConstructor.java in chapter09

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_09/DuckConstructor.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_09/DuckConstructor.java
@@ -4,14 +4,14 @@ package io.github.vijitha_venugopal.head_first_java.chapter_09;
  * class using constuctor
  * Created by vijitha on 05/08/16.
  */
-public class UseADuck {
+public class DuckConstructor {
     public static void main(String[] args) {
         Duck d=new Duck(42);
     }
 }
 class Duck{
-    int size;
-    public Duck(int duckSize) {
+    private int size;
+    Duck(int duckSize) {
         System.out.println("Quack! Quack!");
         size=duckSize;
         System.out.println("Size :"+duckSize);
