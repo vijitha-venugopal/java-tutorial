e9a503c4db0dad0163127144077537e1197f0f08
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-05 10:22:07 +0530

    Created TestBank.java  in chapter_07 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_07/TestBank.java
@@ -0,0 +1,28 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_07;
+
+/**
+ * Created by vijitha on 05/08/16.
+ */
+
+
+class TestBank{
+    public static void main(String args[]){
+        Bank b=new SBI();
+        Bank b1=new PNB();
+        int interest=b.getRateOfInterest();
+        int interest1=b1.getRateOfInterest();
+        System.out.println("Rate of Interest is: "+interest+" %");
+        System.out.println("Rate of Interest is: "+interest1+" %");
+
+    }
+}
+abstract class Bank{
+    abstract int getRateOfInterest();
+}
+
+class SBI extends Bank{
+    int getRateOfInterest(){return 7;}
+}
+class PNB extends Bank{
+    int getRateOfInterest(){return 10;}
+}
