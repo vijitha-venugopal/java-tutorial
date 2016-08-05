b6c3039420e490919bfca37ae6980d85863943af
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-05 11:28:27 +0530

    Created AnimalTestDrive.java  in chapter_08 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_08/AnimalTestDrive.java
@@ -0,0 +1,64 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_08;
+
+/**
+ * class using method in objects
+ * Created by vijitha on 05/08/16.
+ */
+
+
+import java.util.ArrayList;
+
+public class AnimalTestDrive {
+    public static void main(String[] args) {
+        ArrayList<String> pets = new ArrayList<String>();
+        pets.add("Parrot");
+        pets.add("Dog");
+        pets.add("Cat");
+
+        ArrayList<String> fruits = new ArrayList<String>();
+        fruits.add("Apple");
+        fruits.add("Orange");
+        fruits.add("Grape");
+
+        ArrayList<String> animals = new ArrayList<String>();
+        animals.add("Parrot");
+        animals.add("Dog");
+        animals.add("Cat");
+
+        System.out.println("contain :" + pets);
+        System.out.println("contain :" + fruits);
+        System.out.println("contain :" + animals);
+
+        int thesize = pets.size();
+        System.out.println("Size :" + thesize);
+        boolean isin = pets.contains("Cat");
+        System.out.println("Cat contains in pets :" + isin);
+        int idx = pets.indexOf("Cat");
+        System.out.println("Index of cat :" + idx);
+        boolean empty = pets.isEmpty();
+        System.out.println("Is it Pets empty ? :" + empty);
+
+
+        AnimalTestDrive obj = new AnimalTestDrive();
+        System.out.println("Class Name" + " ' " + obj.getClass() + " ' ");
+
+        if (pets.equals(animals)) {
+            System.out.println("Pets and animals are Equal  ");
+        }
+        else {
+            System.out.println("Pets and animals are not Equal  ");
+        }
+
+        if (fruits.equals(animals)) {
+            System.out.println("Fruits and animals are Equal  ");
+        }
+        else {
+            System.out.println("Pets and animals are not Equal  ");
+        }
+
+        System.out.println("hashcode :" + " ' " + obj.hashCode() + " ' ");
+
+        System.out.println("ToString :" + " ' " + obj.toString() + " ' ");
+
+    }
+}
