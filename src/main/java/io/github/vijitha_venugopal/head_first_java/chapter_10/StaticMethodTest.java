4533e9a070b593ff44ba76e56a634307ab9d446d
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-05 18:12:49 +0530

    Created StaticMethodTest.java in chapter_10 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_10/StaticMethodTest.java
@@ -0,0 +1,26 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_10;
+
+/**
+ * class with static method
+ * Created by vijitha on 05/08/16.
+ */
+public class StaticMethodTest {
+    public static void main(String[] args) {
+        display();  //calling without using object
+        show();
+
+        StaticMethodTest t = new StaticMethodTest();
+        t.run();  //calling using object
+    }
+
+    private static void display() {
+        System.out.println("Programming is amazing.");
+    }
+
+    private static void show(){
+        System.out.println("Java is awesome.");
+    }
+    private void run(){
+        System.out.println("Java is powerful.");
+    }
+}
