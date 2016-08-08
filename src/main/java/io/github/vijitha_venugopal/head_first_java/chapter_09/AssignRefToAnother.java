a3205f818139c5c678a30fc06d5c3c024d6a14e9
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-08 10:02:02 +0530

    Created AssignRefToAnother.java in chapter_09 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_09/AssignRefToAnother.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_09/AssignRefToAnother.java
@@ -7,19 +7,19 @@ package io.github.vijitha_venugopal.head_first_java.chapter_09;
 public class AssignRefToAnother {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
-        Rectangle r2 = r1;
 
         r1.length = 10;
-        r2.length = 20;
+        r1.length = 20;
 
         System.out.println("Value of R1's Length : " + r1.length);
-        System.out.println("Value of R2's Length : " + r2.length);
+        System.out.println("Value of R2's Length : " + r1.length);
     }
 }
 
 
 class Rectangle {
     double length;
+
     double breadth;
 }
 
