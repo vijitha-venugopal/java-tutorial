e4298f8aab59b771a4f228d0ee8fe226b1f9b986
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-08 14:51:27 +0530

    Created StsticimportsTest.java in chapter_10 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_10/StaticImportsTest.java
@@ -0,0 +1,14 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_10;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+/**
+ * Created by vijitha on 08/08/16.
+ */
+public class StaticImportsTest {
+    public static void main(String[] args) {
+
+        System.out.println("tan 60 : "+tan(60));
+        out.println("sqrt of 2.0 : "+sqrt(2.0));
+    }
+}
