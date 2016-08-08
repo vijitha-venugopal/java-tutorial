4322f0fd63ce4ba03be8aada3a21f17ba49cb742
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-08 10:11:03 +0530

    Created A.java in chapter_10 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_10/A.java
@@ -0,0 +1,14 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_10;
+
+/**
+ * class for static final variable
+ * Created by vijitha on 08/08/16.
+ */
+
+class A{
+    private static final int data=50;
+    public static void main(String args[]){
+        System.out.println(A.data);
+    }
+}
+
