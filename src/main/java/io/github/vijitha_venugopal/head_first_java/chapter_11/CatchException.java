867f44acb88b194652f37d426f8213b9a7b4edac
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-08 18:56:11 +0530

    Updated TestExceptions.java in chapter_11 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_11/CatchException.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_11/CatchException.java
@@ -3,16 +3,18 @@ package io.github.vijitha_venugopal.head_first_java.chapter_11;
  * class using catch keyword
  * Created by vijitha on 08/08/16.
  */
-public class CatchException {
-    private static int sum(int num1, int num2){
-        if (num1 == 0)
-            throw new ArithmeticException("First parameter is not valid");
+public class CatchException   {
+    private static int sum(int num1, int num2)throws ArithmeticException {
+        if (num2 == 0) {
+            throw new ArithmeticException("second parameter is zero can,t find division");
+        }
+
         else
             System.out.println("Answer is : ");
-        return num1+num2;
+            return num1/num2;
     }
     public static void main(String args[]){
-        int res=sum(0,12);
+        int res=sum(1,1);
         System.out.println(res);
         System.out.println("Continue Next statements");
     }
