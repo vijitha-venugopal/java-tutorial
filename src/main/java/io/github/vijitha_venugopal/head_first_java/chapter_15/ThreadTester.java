47ce48d8dc22949ce946cfd94d77d40b7f6ab613
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-10 19:07:32 +0530

    Updated ThreadTester.java in chapter_15 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_15/ThreadTester.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_15/ThreadTester.java
@@ -5,28 +5,24 @@ import java.lang.Runnable;
  * class using thread
  * Created by vijitha on 10/08/16.
  */
-public class ThreadTester{
+class MyRunnable implements Runnable {
+    public void run(){
+        go();
+    }
+    public void go() {
+        doMore();
+    }
+
+    private void doMore(){
+        System.out.println("top o' the stack");
+    }
+}
+class ThreadTester{
     public static void main(String[] args) {
         Runnable threadJob= new MyRunnable();
         Thread myThread=new Thread(threadJob);
         myThread.start();
 
         System.out.println("back in main");
-    }
-}
-class MyRunnable implements Runnable {
-    public void run(){
-        go();
-    }
-    public void go() {
-        System.out.println("go method");
-        more();
-    }
-    private void more(){
-        System.out.println("more method");
-        doMore();
-    }
-    private void doMore(){
-        System.out.println("doMore method");
     }
 }
