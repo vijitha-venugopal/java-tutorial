0dd0da8b5eb0f828ecdce0652ccf19d0d8058091
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-10 16:29:13 +0530

    Created DailyAdviceServer.java in chapter_15 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_15/DailyAdviceServer.java
@@ -0,0 +1,49 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_15;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * class for serversocket and waits client request
+ * Created by vijitha on 10/08/16.
+ */
+public class DailyAdviceServer
+{
+    private String[] adviceList = {"Take smaller bites", "Go for the tight jeans. No they do NOT make you look fat",
+            "One word: inappropriate", "Just for today, be honest.  Tell your boss what you *really* think",
+            "You might want to rethink that haircut"};
+
+    private void go() {
+        try {
+            ServerSocket serverSock = new ServerSocket(4242);
+            while (true)
+            {
+                Socket sock = serverSock.accept();
+
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String advice = getAdvice();
+                writer.println(advice);
+                writer.close();
+                System.out.println(advice);
+            }
+        } catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+    private String getAdvice() {
+        int random = (int) (Math.random() * adviceList.length);
+        return adviceList[random];
+    }
+
+    public static void main(String[] args)
+    {
+        DailyAdviceServer server = new DailyAdviceServer();
+        server.go();
+    }
+
+}
