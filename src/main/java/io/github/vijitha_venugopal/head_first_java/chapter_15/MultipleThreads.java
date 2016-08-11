bd6b12f8202a338f8c89f2db3a9aea20f50feff9
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-11 10:41:50 +0530

    Created MultipleThreads.java in chapter_15 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/chapter_15/MultipleThreads.java
@@ -0,0 +1,27 @@
+package io.github.vijitha_venugopal.head_first_java.chapter_15;
+import java.lang.Runnable;
+/**
+ * class for multiple threads
+ * Created by vijitha on 11/08/16.
+ */
+
+
+class MultipleThreads implements Runnable{
+    public static void main(String[] args) {
+        MultipleThreads runner=new MultipleThreads();
+        Runnable threadJob= new MyRunnable();
+        Thread alpha=new Thread(runner);
+        Thread beta=new Thread(runner);
+        alpha.setName("Alpha thread");
+        beta.setName("Beta thread");
+        alpha.start();
+        beta.start();
+    }
+
+    public void run() {
+        for (int i=0;i<5;i++){
+            String threadName=Thread.currentThread().getName();
+            System.out.println(threadName+" is running");
+        }
+    }
+}
