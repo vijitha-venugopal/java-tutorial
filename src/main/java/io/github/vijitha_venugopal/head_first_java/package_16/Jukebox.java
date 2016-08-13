bc8ba2804146ba8c2a8c50ccafec3eca97749d2f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-13 09:52:31 +0530

    Updated Jukebox.java in chapter_16 package

--- src/main/java/io/github/vijitha_venugopal/head_first_java/package_16/Jukebox.java
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/package_16/Jukebox.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * class using arraylist
  * Created by vijitha on 12/08/16.
  */
 public class Jukebox {
-    private ArrayList<String> songList = new ArrayList<String>();
+    ArrayList<String> songList = new ArrayList<String>();
 
     public static void main(String[] args) {
         new Jukebox().go();
@@ -19,20 +20,22 @@ public class Jukebox {
     public void go() {
         getSongs();
         System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println("After sort : "+songList);
     }
 
-    private void getSongs() {
+    void getSongs() {
         try {
             File file = new File("SongList.txt");
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line;
+            String line = null;
             while ((line = reader.readLine()) != null) {
                 addSong(line);
             }
         } catch (Exception ex) { ex.printStackTrace(); }
     }
 
-    private void addSong(String lineToParse) {
+    void addSong(String lineToParse) {
         String[]tokens = lineToParse.split("/");
         songList.add(tokens[0]);
     }
