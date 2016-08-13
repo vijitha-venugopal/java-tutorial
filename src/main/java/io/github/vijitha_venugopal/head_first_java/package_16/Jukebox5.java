a127d4597b17eace38242892b55c20e8615a0463
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-13 11:04:56 +0530

    Created Jukebox5.java in chapter_16 package

---
+++ src/main/java/io/github/vijitha_venugopal/head_first_java/package_16/Jukebox5.java
@@ -0,0 +1,58 @@
+package io.github.vijitha_venugopal.head_first_java.package_16;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * class using compare objects
+ * Created by vijitha on 13/08/16.
+ */
+public class Jukebox5 {
+
+
+    ArrayList<Song> songList = new ArrayList<Song>();
+    public static void main(String[] args) {
+        new Jukebox5().go();
+
+    }
+
+    class ArtistCompare implements Comparator<Song> {
+        public int compare(Song one, Song two) {
+            return one.getArtist().compareTo(two.getArtist());
+        }
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+
+        ArtistCompare artistCompare = new ArtistCompare();
+        Collections.sort(songList, artistCompare);
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("SongLst.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
+    }
+}
