1a8c6ae23d33e9f93630c53cd4085bb46896b33a
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 12:11:33 +0530

    Created TypeOperator.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/TypeOperator.js
@@ -0,0 +1,18 @@
+/**
+ * type operator
+ * Created by vijitha on 17/08/16.
+ */
+var a = 10;
+var b = "String";
+var linebreak = "<br />";
+
+result = (typeof a );
+document.write("Result => ");
+document.write(result);
+document.write(linebreak);
+
+result = (typeof b );
+document.write("Result => ");
+document.write(result);
+document.write(linebreak);
+
