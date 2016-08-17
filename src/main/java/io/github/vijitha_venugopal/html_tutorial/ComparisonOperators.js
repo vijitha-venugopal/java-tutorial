c41e2af61ea29f67efabe0f11ca437bf030deb23
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 11:12:20 +0530

    Created ComparisonOperators.js in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/ComparisonOperators.js
@@ -0,0 +1,44 @@
+/**
+ * Comparison operatins
+ * Created by vijitha on 17/08/16.
+ */
+var a = 10;
+var b = 20;
+var linebreak = "<br />";
+
+document.write("a = ");
+document.write(a);
+document.write(linebreak);
+document.write("b = ");
+document.write(b);
+document.write(linebreak);
+
+document.write("(a == b) => ");
+result = (a == b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a != b) => ");
+result = (a != b);
+document.write(result);
+document.write(linebreak);
+
+document.write("( a < b) =>");
+result=(a<b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a > b) => ");
+result = (a > b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a <= b) => ");
+result = (a <= b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a >= b) => ");
+result = (a >= b);
+document.write(result);
+document.write(linebreak);
