05538790b692e305823de97d9311a2ce14943cd6
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 14:20:12 +0530

    Created IfElse.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/IfElse.js
@@ -0,0 +1,12 @@
+/**
+ * if else working
+ * Created by vijitha on 17/08/16.
+ */
+var age = 20;
+
+if( age > 18 ){
+    document.write("<b>Qualifies for driving</b>");
+}
+else {
+    document.write("<b>Not qualifies for driving</b>");
+}
