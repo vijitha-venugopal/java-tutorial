7baa6df2bf75d0bb27bc0f204726ec2677b7433f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 16:57:43 +0530

    Created Objects.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/Objects.js
@@ -0,0 +1,10 @@
+/**
+ * objects
+ * Created by vijitha on 17/08/16.
+ */
+var book = new Object();   // Create the object
+book.subject = "Perl"; // Assign properties to the object
+book.author  = "Mohtashim";
+
+document.write("Book name is : " + book.subject + "<br>");
+document.write("Book author is : " + book.author + "<br>");
