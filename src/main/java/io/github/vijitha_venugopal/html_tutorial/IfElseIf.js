19eefc5651f379241c4a4e0713bdfd489b4f549f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 14:39:36 +0530

    Created IfElseIf.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/IfElseIf.js
@@ -0,0 +1,20 @@
+/**
+ * if elseif working
+ * Created by vijitha on 17/08/16.
+ */
+var book = "maths";
+if( book == "history" ){
+    document.write("<b>History Book</b>");
+}
+
+else if( book == "maths" ){
+    document.write("<b>Maths Book</b>");
+}
+
+else if( book == "economics" ){
+    document.write("<b>Economics Book</b>");
+}
+
+else{
+    document.write("<b>Unknown Book</b>");
+}
