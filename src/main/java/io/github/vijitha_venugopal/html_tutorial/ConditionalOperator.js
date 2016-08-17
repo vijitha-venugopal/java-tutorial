de8470b3304f78e95c16b6175df726854f5b0fb2
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 11:16:16 +0530

    Created ConditionalOperator.js in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/ConditionalOperator.js
@@ -0,0 +1,24 @@
+/**
+ * conditional operations
+ * Created by vijitha on 17/08/16.
+ */
+var a = 10;
+var b = 20;
+var linebreak = "<br />";
+
+document.write(" a=");
+document.write(a);
+document.write(linebreak);
+document.write(" b=");
+document.write(b);
+document.write(linebreak);
+
+document.write ("((a > b) ? 100 : 200) => ");
+result = (a > b) ? 100 : 200;
+document.write(result);
+document.write(linebreak);
+
+document.write ("((a < b) ? 100 : 200) => ");
+result = (a < b) ? 100 : 200;
+document.write(result);
+document.write(linebreak);
