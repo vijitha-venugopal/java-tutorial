b565da09355a9548fd0bf95b8858b02b6b360ebe
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 16:39:23 +0530

    Created VoidKeyword.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/VoidKeyword.js
@@ -0,0 +1,11 @@
+/**
+ * void keyword
+ * Created by vijitha on 17/08/16.
+ */
+
+    function getValue(){
+        var a,b,c;
+        var linebreak="</br>";
+        a = void ( c = 7, b = 5 );
+        document.write('a = ' + a +linebreak+ ' b = ' + b+linebreak +' c = ' + c );
+    }
