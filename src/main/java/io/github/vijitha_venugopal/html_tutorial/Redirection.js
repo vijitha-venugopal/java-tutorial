d0d679e8ea32bd33cf4fadb711035252affc0d02
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 15:56:59 +0530

    Created Redirection.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/Redirection.js
@@ -0,0 +1,10 @@
+/**
+ * Re direction
+ * Created by vijitha on 17/08/16.
+ */
+function Redirect() {
+    window.location="http://www.google.com";
+}
+function Redirect1() {
+    window.location="http://www.opera.com";
+}
