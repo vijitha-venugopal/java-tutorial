22b6c3812b267e4e519b7ed1dfa0a735d8ed9a14
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-17 14:50:39 +0530

    Created ForLoop.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/ForLoop.js
@@ -0,0 +1,13 @@
+/**
+ * for loop
+ * Created by vijitha on 17/08/16.
+ */
+var count;
+document.write("Starting Loop" + "<br />");
+
+for(count = 0; count < 10; count++){
+    document.write("Current Count : " + count );
+    document.write("<br />");
+}
+
+document.write("Loop stopped!");
