8499036a8dcc1b60a2dc4f84c7b778dc5c5d8f84
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-18 16:25:39 +0530

    Created JSMathConstants.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/JSMathConstants.js
@@ -0,0 +1,14 @@
+/**
+ * js math constants
+ * Created by vijitha on 18/08/16.
+ */
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+        Math.E + "<br>" +
+        Math.PI + "<br>" +
+        Math.SQRT2 + "<br>" +
+        Math.SQRT1_2 + "<br>" +
+        Math.LN2 + "<br>" +
+        Math.LN10 + "<br>" +
+        Math.LOG2E + "<br>" +
+        Math.LOG10E + "<br>";}
