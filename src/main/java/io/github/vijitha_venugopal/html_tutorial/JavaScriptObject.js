1b92b061010ed8ae186e19024b1bbf613f2c83fc
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-18 10:15:40 +0530

    Created JavaScriptObject.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/JavaScriptObject.js
@@ -0,0 +1,6 @@
+/**
+ * object assign many values
+ * Created by vijitha on 18/08/16.
+ */
+var car = {type:"Fiat", model:"500", color:"white"};
+document.getElementById("demo").innerHTML = car.type;
