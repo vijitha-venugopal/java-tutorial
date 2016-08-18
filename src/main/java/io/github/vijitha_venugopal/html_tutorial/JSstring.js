ed17dc3c365c6f74376dd7ad377e5c9cab2f3b4f
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-18 12:59:59 +0530

    Created JSstring.js  in  package javascript_tutorial

--- src/main/java/io/github/vijitha_venugopal/html_tutorial/JSstring.js
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/JSstring.js
@@ -11,4 +11,9 @@ document.getElementById("demo").innerHTML ="text1 : "+
     txt2+"<br>"+"text length: "+txt2.length+"<br>"+"typeof text1 :"+
     typeof(txt)+"<br>"+"typeof text2 :"+
     typeof(txt1)+"<br>"+"typeof text3 :"+
-    typeof(txt2)+"<br>"+"text2 and text3 are same ? : "+(txt1==txt2);
+    typeof(txt2)+"<br>"+"text2 and text3 are same ? : "+
+    (txt1==txt2)+"<br>"+"Index of G in text1 : "+
+    txt.indexOf("G")+"<br>"+"Search G in text1 :"+
+    txt.search("G")+"<br>"+"After Slice text1 : "+
+    txt.slice(1,10)+"<br>"+"Substring of ABCD : "+
+    txt1.substring(1,3)+"<br>"+"Substr of ABCD : "+txt1.substr(1,2)+"<br>"+"replace ABCD as ABBD: "+txt1.replace("BC","BB");
