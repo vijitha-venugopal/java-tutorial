b0a5edd53aade5a545e54dd7988e555d8369fabe
Vijitha Venugopal <vijithavenugopal1992@gmail.com>
2016-08-18 16:07:54 +0530

    Created JSMath.js  in  package javascript_tutorial

---
+++ src/main/java/io/github/vijitha_venugopal/html_tutorial/JSMath.js
@@ -0,0 +1,30 @@
+/**
+ * Java script math
+ * Created by vijitha on 18/08/16.
+ */
+
+function min() {
+
+    document.getElementById("demo").innerHTML ="min(0, 150, 30, 20, -8, -200)  ----->>"+
+        Math.min(0, 150, 30, 20, -8, -200);}
+function max() {
+
+    document.getElementById("demo").innerHTML ="max(0, 150, 30, 20, -8, -200   ----->>)"+
+        Math.max(0, 150, 30, 20, -8, -200);}
+function random() {
+
+    document.getElementById("demo").innerHTML ="random ----->>)"+
+        Math.random();}
+function round() {
+
+    document.getElementById("demo").innerHTML ="round(25.369)   ----->>"+
+        Math.round(25.369);}
+function ceil() {
+
+    document.getElementById("demo").innerHTML ="ceil(25.369)   ----->>"+
+        Math.ceil(25.369);}
+function floor() {
+
+    document.getElementById("demo").innerHTML = "floor(4.71)   ----->>"+Math.floor(4.71);}
+
+
